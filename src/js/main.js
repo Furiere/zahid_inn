95f4f3dd05fb401bf85931b4276fbc3f3dbd2eca
Furiere <denis.boldin.hse@gmail.com>
2026-08-31 22:09:59 +0500

    Update the ru part to comply with the eng.

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ import { initNav } from './nav.js';
 import { initOfferPopup } from './offer-popup.js';
 import { initHeroCarousel } from './hero-carousel.js';
 import { initRatesCarousel } from './rates-carousel.js';
+import { initRoomCarousels } from './room-carousel.js';
 
 initNav();
 initOfferPopup();
 initHeroCarousel();
 initRatesCarousel();
+initRoomCarousels();
